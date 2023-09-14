aef9b169900ef976d07e448b6b9c8c89c458b488
Dong Ngo - Dev Abaha 04 <devabaha04@gmail.com>
2023-09-14 12:20:15 +0700

    loading

--- sun_ray/TheSun.jsx
+++ sun_ray/TheSun.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {
   Animated,
   Button,
@@ -8,120 +8,130 @@ import {
   View,
   Easing,
 } from 'react-native';
+import SunRay from './SunRay';
 
-const ARR = [1, 2, 3, 4];
-const WIDTH = 100;
-const SUB_WIDTH = 20;
-const R = WIDTH / 2;
-const ANGLE = (2 * Math.PI) / ARR.length;
+const r = 50;
+const sunRayWidth = 20;
+const sunRayHeight = 40;
+const duration = 1000;
+const sunRays = Array.from({ length: (2 * (Math.PI * r)) / sunRayWidth });
 export default function TheSun() {
-  const animation = useRef(new Animated.Value(0)).current;
-  const [btnClicked, setBtnClicked] = useState(false);
-  const [animatedRotates] = useState(ARR.map(() => new Animated.Value(0)));
-  const startAnimation = () => {
-    // animatedRotates
-    //   .map((animation, index) => {
-    Animated.timing(animation, {
-      toValue: btnClicked ? 1 : 0,
-      easing: Easing.linear,
-      useNativeDriver: true,
-      // });
-    }).start();
-  };
-  const sunRayStyle = (index) => {
-    const base = R - SUB_WIDTH / 2;
-    const x = base - (R + SUB_WIDTH / 2 + 10) * Math.sin(ANGLE * index);
-    const y = base - (R + SUB_WIDTH / 2 + 10) * Math.cos(ANGLE * index);
+  const [animatedRotates] = useState(sunRays.map(() => new Animated.Value(0)));
+  const [animatedTranslates] = useState(
+    sunRays.map(() => new Animated.Value(0)),
+  );
+  const [animatedSunSpin] = useState(new Animated.Value(0));
+  const [isShow, setShow] = useState(false);
 
-    return {
-      top: y,
-      left: x,
-      // transform: [{ rotate: `${180 - index * 90}deg` }],
-    };
+  const toggle = () => {
+    setShow((prevValue) => !prevValue);
   };
+
   const renderSunRay = () => {
-    return ARR.map((item, index) => {
+    return sunRays.map((_, index) => {
       return (
-        <Animated.View
-          style={[
-            styles.sunRay,
-            sunRayStyle(index),
-            {
-              transform: [
-                {
-                  rotate: animation.interpolate({
-                    inputRange: [0, 1],
-                    outputRange: [
-                      `${0 - index * 90}deg`,
-                      `${180 - index * 90}deg`,
-                    ],
-                  }),
-                },
-              ],
-            },
-          ]}
-          key={index}></Animated.View>
+        <SunRay
+          key={index}
+          index={index}
+          r={r}
+          sunRayHeight={sunRayHeight}
+          sunRayWidth={sunRayWidth}
+          animatedRotateValue={animatedRotates[index]}
+          animatedTranslateValue={animatedTranslates[index]}
+          totalSunRays={sunRays.length}
+        />
       );
     });
   };
+
+  useEffect(() => {
+    animatedRotates.map((animation, index) => {
+      const animations = [
+        Animated.timing(animation, {
+          toValue: isShow ? 1 : 0,
+          duration,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+        Animated.timing(animatedTranslates[index], {
+          toValue: isShow ? 1 : 0,
+          duration,
+          easing: Easing.linear,
+          useNativeDriver: true,
+        }),
+      ];
+
+      return Animated.parallel(animations).start(({ finished }) => {
+        Animated.spring(animatedSunSpin, {
+          toValue: isShow && finished ? 1 : 0,
+          useNativeDriver: true,
+        }).start();
+      });
+    });
+  }, [isShow]);
   return (
-    <View style={styles.mainContainer}>
-      <View style={styles.sunItem}>
-        <Text style={styles.sunText}>The Sun</Text>
+    <View style={styles.wrapper}>
+      <Animated.View
+        style={[
+          styles.container,
+          {
+            transform: [
+              {
+                scale: animatedSunSpin.interpolate({
+                  inputRange: [0, 0.3, 1],
+                  outputRange: [1, 1.2, 1],
+                }),
+              },
+              {
+                rotate: animatedSunSpin.interpolate({
+                  inputRange: [0, 1],
+                  outputRange: ['0deg', 360 * 2 + 'deg'],
+                }),
+              },
+            ],
+          },
+        ]}>
         {renderSunRay()}
-      </View>
-      <TouchableOpacity
-        style={styles.btnItem}
-        onPress={() => {
-          setBtnClicked(!btnClicked);
-          startAnimation();
-        }}>
-        <Text>Click</Text>
-      </TouchableOpacity>
+
+        <View style={styles.innerSunContainer}>
+          <View style={styles.innerSunBaseStyle} />
+          <Text style={styles.sunTitle}>Sun</Text>
+        </View>
+      </Animated.View>
+
+      <Button title="Toggle" onPress={toggle} />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  mainContainer: {
+  wrapper: {
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
   },
-  sunItem: {
-    width: WIDTH,
-    height: WIDTH,
-    backgroundColor: '#fdd835',
-    borderRadius: R,
-    // justifyContent: 'center',
-    // alignItems: 'center',
-    position: 'relative',
+  container: {
+    alignItems: 'center',
+    marginBottom: 50,
+    width: r * 2,
+    height: r * 2,
   },
-  sunText: {
-    top: '42%',
-    left: '20%',
+  innerSunContainer: {
+    borderRadius: r,
+    width: '100%',
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-
-  sunRay: {
+  innerSunBaseStyle: {
+    borderRadius: r,
+    width: '80%',
+    height: '80%',
+    backgroundColor: '#ffea00',
     position: 'absolute',
-    width: SUB_WIDTH,
-    height: SUB_WIDTH,
-    backgroundColor: 'transparent',
-    borderLeftWidth: SUB_WIDTH / 2,
-    borderRightWidth: SUB_WIDTH / 2,
-    borderTopWidth: SUB_WIDTH,
-    borderLeftColor: 'transparent',
-    borderRightColor: 'transparent',
-    borderTopColor: 'red',
-    // backgroundColor: 'red',
-    // borderRadius: SUB_WIDTH / 2,
   },
-  btnItem: {
-    marginTop: 100,
-    borderWidth: 1,
-    paddingHorizontal: 20,
-    paddingVertical: 6,
-    borderRadius: 10,
-    backgroundColor: '#96c267',
+  sunTitle: {
+    color: 'purple',
+    fontWeight: 'bold',
   },
 });
